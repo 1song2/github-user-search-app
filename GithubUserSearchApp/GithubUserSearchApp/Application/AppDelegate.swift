7476c1166f9b54f381264810a045e9d2ace1e142
1song2 <1song2918@gmail.com>
2021-10-24 22:33:38 +0900

    feat(layout): set root view controller programmatically (#5)

--- GithubUserSearchApp/GithubUserSearchApp/Application/AppDelegate.swift
+++ GithubUserSearchApp/GithubUserSearchApp/Application/AppDelegate.swift
@@ -11,4 +11,14 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController()
+        window?.rootViewController = navigationController
+        navigationController.pushViewController(ViewController(), animated: false)
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
 }
