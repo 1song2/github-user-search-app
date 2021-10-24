46adee65a2739ba1316d92fd735583948657f557
1song2 <1song2918@gmail.com>
2021-10-24 16:55:17 +0900

    feature(network): create Endpoint (#2)

---
+++ GithubUserSearchApp/GithubUserSearchApp/Infrastructure/Network/Endpoint.swift
@@ -0,0 +1,100 @@
+//
+//  Endpoint.swift
+//  GithubUserSearchApp
+//
+//  Created by Song on 2021/10/24.
+//
+
+import Foundation
+
+enum HTTPMethodType: String {
+    case get = "GET"
+}
+
+class Endpoint<R>: ResponseRequestable {
+    typealias Response = R
+    
+    let path: String
+    let isFullPath: Bool
+    let method: HTTPMethodType
+    let headerParameters: [String: String]
+    let queryParametersEncodable: Encodable?
+    
+    init(path: String,
+         isFullPath: Bool = false,
+         method: HTTPMethodType,
+         headerParameters: [String: String] = [:],
+         queryParametersEncodable: Encodable? = nil) {
+        self.path = path
+        self.isFullPath = isFullPath
+        self.method = method
+        self.headerParameters = headerParameters
+        self.queryParametersEncodable = queryParametersEncodable
+    }
+}
+
+protocol Requestable {
+    var path: String { get }
+    var isFullPath: Bool { get }
+    var method: HTTPMethodType { get }
+    var headerParameters: [String: String] { get }
+    var queryParametersEncodable: Encodable? { get }
+    
+    func urlRequest(with networkConfig: NetworkConfigurable) throws -> URLRequest
+}
+
+protocol ResponseRequestable: Requestable {
+    associatedtype Response
+}
+
+enum RequestGenerationError: Error {
+    case components
+}
+
+extension Requestable {
+    func url(with config: NetworkConfigurable) throws -> URL {
+        
+        let baseURL = config.baseURL.absoluteString.last != "/"
+        ? config.baseURL.absoluteString + "/"
+        : config.baseURL.absoluteString
+        let endpoint = isFullPath ? path : baseURL.appending(path)
+        
+        guard var urlComponents = URLComponents(string: endpoint) else { throw RequestGenerationError.components }
+        var urlQueryItems = [URLQueryItem]()
+        
+        let queryParameters = try queryParametersEncodable?.toDictionary() ?? [:]
+        queryParameters.forEach {
+            urlQueryItems.append(URLQueryItem(name: $0.key, value: "\($0.value)"))
+        }
+        urlComponents.queryItems = !urlQueryItems.isEmpty ? urlQueryItems : nil
+        guard let url = urlComponents.url else { throw RequestGenerationError.components }
+        return url
+    }
+    
+    func urlRequest(with config: NetworkConfigurable) throws -> URLRequest {
+        let url = try self.url(with: config)
+        var urlRequest = URLRequest(url: url)
+        var allHeaders: [String: String] = [:]
+        headerParameters.forEach { allHeaders.updateValue($1, forKey: $0) }
+        
+        urlRequest.httpMethod = method.rawValue
+        urlRequest.allHTTPHeaderFields = allHeaders
+        return urlRequest
+    }
+}
+
+private extension Dictionary {
+    var queryString: String {
+        return self.map { "\($0.key)=\($0.value)" }
+        .joined(separator: "&")
+        .addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) ?? ""
+    }
+}
+
+private extension Encodable {
+    func toDictionary() throws -> [String: Any]? {
+        let data = try JSONEncoder().encode(self)
+        let josnData = try JSONSerialization.jsonObject(with: data)
+        return josnData as? [String : Any]
+    }
+}
