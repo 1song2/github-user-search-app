631e07d5fd4cd11651d71620bc031a64b75878ba
1song2 <1song2918@gmail.com>
2021-10-24 22:40:59 +0900

    feat(layout): set background color and nav bar (#5)
    - Enable large titles
    - Add search controller
    - Show scope bar

--- GithubUserSearchApp/GithubUserSearchApp/Presentation/ViewController.swift
+++ GithubUserSearchApp/GithubUserSearchApp/Presentation/ViewController.swift
@@ -8,8 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    //MARK: - Private
+    
+    private func setupViews() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "GitHub Users" // viewModel.screenTitle
+        if #available(iOS 13, *) {
+            view.backgroundColor = .systemBackground
+        } else {
+            view.backgroundColor = .white
+        }
+        setupSearchController()
+    }
+    
+    private func setupSearchController() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.showsScopeBar = true
+        searchController.searchBar.scopeButtonTitles = [ // viewModel.buttonTitles
+            NSLocalizedString("API", comment: ""),
+            NSLocalizedString("Local", comment: "")
+        ]
     }
 }
