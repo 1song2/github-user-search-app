f0235eef7e3e9f839201c4c5229d6aec500fcdb3
1song2 <1song2918@gmail.com>
2021-10-31 12:04:42 +0900

    refactor(presentation): make view model hashable (#6)
    - Import RxSwift and RxRelay
    - Add method to change isStarred value

--- GithubUserSearchApp/GithubUserSearchApp/Presentation/ViewModel/UserViewModel.swift
+++ GithubUserSearchApp/GithubUserSearchApp/Presentation/ViewModel/UserViewModel.swift
@@ -6,15 +6,31 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
 
-struct UserViewModel {
+class UserViewModel: Hashable {
+    let id: Int
     let username: String
     let avatarUrl: String
-    var isStarred: Bool
+    var isStarred: BehaviorRelay<Bool>
     
     init(user: User) {
+        self.id = user.id
         self.username = user.username
         self.avatarUrl = user.avatarUrl
-        self.isStarred = user.isStarred
+        self.isStarred = BehaviorRelay<Bool>(value: user.isStarred)
+    }
+    
+    func didStar(_ bool: Bool) {
+        isStarred.accept(bool)
+    }
+    
+    static func == (lhs: UserViewModel, rhs: UserViewModel) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
