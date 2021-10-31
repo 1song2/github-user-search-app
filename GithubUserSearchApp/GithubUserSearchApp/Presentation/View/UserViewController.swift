6e59638a9dd7224b4b82b7cbd3ae773e495ccf62
1song2 <1song2918@gmail.com>
2021-10-31 14:58:33 +0900

    feat(presentation): set up bindings
    - Define alphabetical data source to show users alphabetically

--- GithubUserSearchApp/GithubUserSearchApp/Presentation/View/UserViewController.swift
+++ GithubUserSearchApp/GithubUserSearchApp/Presentation/View/UserViewController.swift
@@ -6,9 +6,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class UserViewController: UITableViewController {
+    private var viewModel: UsersViewModel!
+    private var avatarImagesRepository: AvatarImagesRepository?
+    private var disposeBag = DisposeBag()
     private var searchController = UISearchController(searchResultsController: nil)
+    typealias AlphabeticalSectionModel = SectionModel<String, UserViewModel>
+    typealias AlphabeticalDataSource = RxTableViewSectionedReloadDataSource<AlphabeticalSectionModel>
+    private var alphabeticalDataSource: AlphabeticalDataSource {
+        let configureCell: (TableViewSectionedDataSource<AlphabeticalSectionModel>,
+                            UITableView, IndexPath, UserViewModel) -> UITableViewCell = { [weak self] (datasource, tableView, indexPath, item) in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: UserCell.reuseIdentifier,
+                                                           for: indexPath) as? UserCell else { return UITableViewCell() }
+            self?.configureCell(cell, with: item)
+            return cell
+        }
+        
+        let datasource = AlphabeticalDataSource(configureCell: configureCell)
+        datasource.titleForHeaderInSection = { datasource, index in
+            return datasource.sectionModels[index].model
+        }
+        return datasource
+    }
     
     static func create(with viewModel: UsersViewModel,
                        avatarImagesRepository: AvatarImagesRepository?) -> UserViewController {
@@ -21,6 +44,7 @@ class UserViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupBindings()
     }
     
     // MARK: - Private
@@ -38,6 +62,40 @@ class UserViewController: UITableViewController {
         setupSearchController()
     }
     
+    private func setupBindings() {
+        searchController.searchBar.rx.selectedScopeButtonIndex.asObservable()
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.viewModel.didChangeSegment($0)
+                self.tableView.dataSource = nil
+                if $0 == 0 {
+                    self.viewModel.allItems
+                        .bind(to: self.tableView.rx.items(cellIdentifier: UserCell.reuseIdentifier,
+                                                          cellType: UserCell.self)) { [weak self] _, item, cell in
+                            self?.configureCell(cell, with: item)
+                        }.disposed(by: self.disposeBag)
+                } else {
+                    let sections = Observable.just(
+                        self.viewModel.starredItems.value
+                            .sorted { $0.key < $1.key }
+                            .map { SectionModel<String, UserViewModel>(model: $0, items: $1) }
+                    )
+                    sections.bind(to: self.tableView.rx.items(dataSource: self.alphabeticalDataSource))
+                        .disposed(by: self.disposeBag)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: {
+                let alert = UIAlertController(title: "Error", message: $0, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true)
+            })
+            .disposed(by: disposeBag)
+    }
+    
     private func setupSearchController() {
         searchController.searchBar.delegate = self
         navigationItem.searchController = searchController
@@ -45,6 +103,21 @@ class UserViewController: UITableViewController {
         searchController.searchBar.showsScopeBar = true
         searchController.searchBar.scopeButtonTitles = viewModel.buttonTitles
         definesPresentationContext = true
+    }
+    
+    private func configureCell(_ cell: UserCell, with userViewModel: UserViewModel) {
+        cell.usernameLabel.text = userViewModel.username
+        cell.updateAvatarImage(with: userViewModel, avatarImagesRepository: avatarImagesRepository)
+        cell.onToggled = {
+            userViewModel.didStar($0)
+            self.viewModel.didStar(userViewModel)
+        }
+        userViewModel.isStarred
+            .subscribe(onNext: {
+                let imageName = $0 ? "star-fill" : "star"
+                cell.starButton.setImage(UIImage(named: imageName), for: [])
+            })
+            .disposed(by: cell.disposeBag)
     }
 }
 
